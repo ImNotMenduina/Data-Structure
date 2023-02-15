ede92bafba61839892bbda7b7784ebc2c0ed083b
ImNotMenduina <mynameis_menduina@outlook.com>
2023-02-14 21:56:17 -0300

    Exercicio de ed1 sobre lista simplesmente encadeada

--- Lista_Encadeada/EXERCICIO_01.c
+++ Lista_Encadeada/EXERCICIO_01.c
@@ -3,76 +3,78 @@
 #include<string.h>
 
 struct lista {
-   //char nome[81];
+    char nome[81];
     float nota;
-    //struct lista *prox;
+    struct lista *prox;
 };
 typedef struct lista LISTA;
 
-/* int aprovados (LISTA** l)
+LISTA* create_list()
 {
-    int count = 0 ; 
-    while(*l != NULL)
-    {
-        if((*l)->nota < 5.0)
-            printf("%s | STATUS:REPROVADO | NOTA:%.1f \n" , (*l)->nome , (*l)->nota) ;
-        else
-            printf("%s | STATUS:APROVADO | NOTA:%.1f \n" , (*l)->nome , (*l)->nota) ;
- 
-    *l = (*l)->prox ; 
-    } 
-} 
-*/
-LISTA** criar_lista()
-{
-    LISTA** lst = malloc(sizeof(LISTA*)) ;
-    *lst = malloc(sizeof(LISTA)) ; 
-    (**lst).nota = 1.0 ; 
-    printf("%f" , (**lst).nota) ; 
-    float x ; 
-    
-    /* printf("%f" , (*(*lst)).nota ) ;  */
-   /*  if(lst != NULL)
-        *lst = NULL ; 
-    return lst ; */ 
+    return NULL ;
 }
-/* 
-void inserir(LISTA** lst)
+
+LISTA* create_student(LISTA* lst)
 {
     char nome[81] ; 
     float nota ; 
+    printf("ALUNO : ") ; 
+    scanf("%s" , nome) ; 
 
-    printf("Insira o nome : ") ; 
-    scanf(" %[^\n]" , nome) ; 
-
-    printf("Insira a nota : ") ; 
-    scanf("%f" , &nota) ; 
+    printf("MEDIA : ") ; 
+    scanf("%f" , &nota) ;
 
     LISTA* novo = (LISTA*) malloc(sizeof(LISTA)) ; 
     strcpy(novo->nome , nome) ; 
     novo->nota = nota ; 
-    novo->prox = *lst ; 
-    *lst = novo ; 
+    novo->prox = lst ; 
+    return novo ; 
+    
 }
- */
+
+void printt(LISTA* lst)
+{
+    LISTA* aux = lst ; 
+    while(aux != NULL)
+    {
+        if(aux->nota < 5.0)
+            printf("</ ALUNO : %s | NOTA : %.1f | STATUS : REPROVADO/> \n" , aux->nome , aux->nota) ; 
+        else
+            printf("</ ALUNO : %s | NOTA : %.1f | STATUS : APROVADO/> \n" , aux->nome , aux->nota) ; 
+
+        aux = aux->prox ; 
+    }
+}
+
 int main()
 {
-    /* LISTA** lst =  */
-    
-    criar_lista() ; 
+    LISTA* lst = create_list() ; 
 
-   
+    int op ; 
 
-   /*  inserir(lst) ; 
-    inserir(lst) ;
-    aprovados(lst) ;  */
+    do
+    {
+        printf("1.(I) 2.(D) 3.(P) 4.(PR) 0.(E)\n//> ") ;
+        scanf("%d" , &op) ; 
 
-    /* int x = 10 ; 
-    int *y = &x ; 
-    int **z = &y ; 
-    printf("%p" , *z) ;
-    printf("%p" , &x) ;  
-    printf("\n") ;  */
+        switch (op)
+        {
+        case 1:
+            lst = create_student(lst) ; 
+            break;
+        case 2:
+            //remove_info(lst) ;
+            break ;
+        case 3:
+            printt(lst) ; 
+            break;
+        case 4:
+            //print_reverse(lst) ; 
+            break ;  
+        default:
+            break;
+        } 
+    } while (op != 0);
 
     return 0 ; 
 }
